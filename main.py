f957260c770cdc09e98f7c515fa75e1665c13764
clara2911 <clara.tump@hotmail.com>
2019-04-05 13:06:44 +0200

    Update main.py
    add plots for loss and accuracy

--- main.py
+++ main.py
@@ -1,47 +1,49 @@
 import numpy as np
 import pickle
+print("imported pickle")
 from ann import ANN
 import matplotlib.pyplot as plt
 
 def main():
   # y = N, // X = dxN // Y=KxN
-  X_train, y_train , Y_train= load_data(batch_file = "data/data_batch_1")
+  X_train, y_train , Y_train = load_data(batch_file = "data/data_batch_1", num=16)
   print("loaded train data")
-  print("X_train: ", X_train.shape)
-  print("Y_train: ", Y_train.shape)
-  X_val, y_val , Y_val = load_data(batch_file = "data/data_batch_2")
-  X_test, y_test , Y_test = load_data(batch_file = "data/test_batch")
+  #X_test, y_test, Y_test = load_data(batch_file = "data/test_batch")
+  print("loaded test batch")
+  X_val, y_val , Y_val = load_data(batch_file = "data/data_batch_2", num=7)
   print("loaded all data sets")
   ann1 = ANN(X_train, Y_train)
   print("initialized ann")
-  #not tested yet
-  cost_history = ann1.train(X_train, Y_train)
+  ann1.train(X_train, Y_train, X_val, Y_val)
   print("cost history; ", cost_history)
-  plt.plot(cost_history)
-  plt.show()
   print("done training")
 
 
-def load_data(batch_file = "data/data_batch_1", num=10):
+
+
+def load_data(batch_file = "data/data_batch_1", num=16):
   with open(batch_file, 'rb') as fo:
     data = pickle.load(fo, encoding='bytes')
     X = preprocess(data[b"data"])
     y = np.array(data[b"labels"])
     Y = one_hot(y)
     print(X.shape)
-  return X[:num].T, y[:num], Y[:num].T
+  if num:
+    return X[:num].T, y[:num], Y[:num].T
+  else:
+    return X, y, Y
 
-def preprocess(dataset):
+def preprocess(chunk):
   """
   Preprocess the data:
   scale from pixel values to values between 0 and 1 and convert to grayscale
   """
-  r=0.3
-  g=0.59
-  b = 0.11
+  #r=0.3
+  #g=0.59
+  #b = 0.11
   # convert to grayscale
-  chunk = dataset.reshape(dataset.shape[0], 1024, 3)
-  chunk = r * chunk[:, :, 0] + g * chunk[:, :,1] + b * chunk[:, :, 2]
+  #chunk = dataset.reshape(dataset.shape[0], 1024, 3)
+  #chunk = r * chunk[:, :, 0] + g * chunk[:, :,1] + b * chunk[:, :, 2]
   # scaling the values
   chunk = chunk / 255.0
   return chunk
