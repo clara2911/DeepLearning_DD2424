d6aae3cd887c83588a241ddf2db349f2f8b88c65
Clara Tump <clara.tump@hotmail.com>
2019-05-29 13:59:28 +0200

    Generate random sequence based on initial settings

--- Assignment 4/main.py
+++ Assignment 4/main.py
@@ -13,17 +13,20 @@ Reads in the data. Runs the RNN, and reports the results.
 
 
 import numpy as np
-from data import Data
-from rnn import RNN
+#from data import Data
+#from rnn import RNN
 
 
 def main():
   data = Data("goblet_book.txt")
   book_chars = data.unique_chars
   book_ints = data.char_to_int(book_chars)
-  print(book_ints)
-  book_chars = data.int_to_char(book_ints)
-  print(book_chars)
+  rnn1 = RNN()
+  x0 = np.random.rand(rnn1.k,1)
+  h0 = np.random.rand(rnn1.m,1)
+  n = 10
+  generated_seq = rnn1.generate(h0, x0, n, book_chars)
+  print("generated sequence: ", generated_seq)
 
 
 
