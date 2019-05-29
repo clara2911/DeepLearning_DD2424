dea122f2b6dfca292570d700c9d2041830fb7d00
Clara Tump <clara.tump@hotmail.com>
2019-05-29 15:34:16 +0200

    Generate onehot output

--- Assignment 4/data.py
+++ Assignment 4/data.py
@@ -18,7 +18,6 @@ class Data:
         self.unique_chars = self._get_unique_chars(self.data_string)
         self.char_to_int_dict, self.int_to_char_dict = self._create_dictionaries(self.unique_chars)
 
-
     def _read_data(self, txtfile):
       """
       reads the txt file and returns the content as a string
@@ -26,6 +25,8 @@ class Data:
       data_string = open(txtfile,'r').read()
       return data_string
 
+
+    
     def _get_unique_chars(self, data_string):
       """
       returns a numpy array of the unique characters in the string of text
@@ -44,7 +45,6 @@ class Data:
       reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
       return dictionary, reverse_dictionary
 
-
     def char_to_int(self, char_list):
       """
       convert a list of characters to the corresponding list of integers
@@ -58,3 +58,23 @@ class Data:
       """
       char_list = [self.int_to_char_dict[int] for int in int_list]
       return char_list
+    
+    def onehot_to_string(self, one_hot_seq):
+      """
+      convert a sequence of characters in one-hot encoded to a string
+      """
+      gen_ints = [np.where(r==1)[0][0] for r in one_hot_seq]
+      gen_char_list = self.int_to_char(gen_ints)
+      generated_text = ''.join(gen_char_list)
+      return generated_text
+
+    def string_to_onehot(self, string_seq):
+      """
+      convert a sequence of characters in one-hot encoded to a string
+      """
+      char_list = list(string_seq)
+      int_list = self.char_to_int(char_list)
+      one_hot = np.zeros((len(self.unique_chars), len(int_list)))
+      for i,int_elem in enumerate(int_list):
+        one_hot[int_elem,i] = 1
+      return one_hot
