980310f4d606c8bf90e2bc854dd23f3039e9ad83
Clara Tump <clara.tump@hotmail.com>
2019-05-30 13:01:11 +0200

    Add forward pass of bptt

--- Assignment 4/rnn.py
+++ Assignment 4/rnn.py
@@ -18,48 +18,80 @@ class RNN:
         self.m = 100               #hidden layer length
         self.k = 80                # number of unique characters
         self.eta = 0.1             # learning rate
-        self.seq_length = 25       # length of the input sequences
-        self.b = np.zeros((self.m,1))   # bias vector
-        self.c = np.zeros((self.k,1))   # another bias vector
-        self.sig = 0.01
+        self.seq_length = 0       # length of the input sequences
+        
+        
         
         # TODO check if u w v is indeed the right order and it shouldnt be u v w
+        self.sig = 0.01
+        self.b = np.zeros((self.m,1))   # bias vector
+        self.c = np.zeros((self.k,1))   # another bias vector
         self.u = np.random.rand(self.m, self.k)*self.sig    # weight matrix 1
         self.w = np.random.rand(self.m, self.m)*self.sig    # weight matrix 2
         self.v = np.random.rand(self.k, self.m)*self.sig    # weight matrix 3
         self.grad_u = 0
         self.grad_w = 0
         self.grad_v = 0
+        self.grad_b = 0
+        self.grad_c = 0
         
-    def generate(self, h0, x0, n, unique_chars):
+    def train(self, X, Y, epochs=1):
       """
-      generate a sequence of n one_hot encoded characters based on initial 
-      hidden state h0 and input character x0
+      Train the RNN using backpropagation through time (bptt)
+      """
+      # now putting this twice. need to find a smarter way
+      self.seq_length = X.shape[1]
+      for i in range(epochs):
+        p, a, h = self.evaluate(X)
+        loss = compute_loss(p, Y)
+        compute_grads = (p, Y, h, a)
+        update_params()
+        print("epoch ", i, " // loss: ", loss)
+        
+    def evaluate(self, X):
+      """
+      evaluates a sequence of one-hot encoded characters X and outputs a 
+      probability vector at each X_t representing the predicted probs
+      for the next character.
+      Used as forward pass of the backpropagation through time (bptt)
+      """
+      # can ignore this when training. or need to find a better place for it
+      self.seq_length = X.shape[1]
       
-      Return: n*k matrix of n generated chars encoded as one-hot vectors
-      """
-      # TODO make sure that this predicts the next char not the current one
-      char_seq = np.zeros((n, self.k))
-      h_prev = h0
-      x_prev = x0
-      # for each of the timesteps in our n-length sequence
-      for t in range(n):
-        # compute probability of current character based on previous character
-        act_curr = np.dot(self.w, h_prev) + np.dot(self.u, x_prev)
-        h_curr = np.tanh(act_curr)
-        o_curr = np.dot(self.v, h_curr) + self.c
-        prob = self.softmax(o_curr)
-        prob = prob.reshape(prob.shape[0],)
+      p = np.zeros((self.seq_length, self.k))
+      a = np.zeros((self.seq_length, self.m))
+      h = np.zeros((self.seq_length, self.m))
+      
+      h_prev = np.zeros((self.m,1))
+      
+      for t in range(self.seq_length):
+        xt = X[:,t].reshape((self.k, 1)) # reshape from (k,) to (k,1)
+        a_curr = np.dot(self.w, h_prev) + np.dot(self.u, xt) + self.b
+        h_curr = np.tanh(a_curr)
+        o_curr = np.dot(self.v, h_prev) + self.c
+        p_curr = self.softmax(o_curr)
         
-        # randomly choose a character for this timestep weighted by the probs
-        predicted_char = self.select_char(prob, unique_chars)
-        char_seq[t] = predicted_char
+        a[t] = a_curr.reshape(self.m) #reshape from (m,1) to (m,)
+        h[t] = h_curr.reshape(self.m) #reshape from (m,1) to (m,)
+        p[t] = p_curr.reshape(self.k) #reshape from (k,1) to (k,)
         
-        # predict char t+1 based on the h and predicted character of timestep t
         h_prev = h_curr
-        x_prev = predicted_char.reshape((predicted_char.shape[0],1))
         
-      return char_seq
+      return p, a, h
+    
+    def compute_loss(p, Y):
+      """
+      Compute the cross entropy loss between:
+      - a (seq_len x k) matrix of predicted probabilities
+      - a (k x seq _len) matrix of true one_hot encoded characters
+      
+      """
+      # todo: check if not one of them needs to be transposed
+      # TODO: this can be more efficient without the for loop
+      loss = 0
+      for t in range(self.seq_length):
+        loss += -np.log(np.dot(Y[:,t].T, p[t])
+      return loss
     
     def softmax(self, Y_pred_lin):
       """
@@ -68,6 +100,25 @@ class RNN:
       ones = np.ones(Y_pred_lin.shape[0])
       Y_pred = np.exp(Y_pred_lin) / np.dot(ones.T, np.exp(Y_pred_lin))
       return Y_pred
+    
+    def compute_grads(self, p, Y, a, h):
+      pass
+    
+    def update_params():
+      pass
+      
+        
+        
+    def generate(self, X, unique_chars):
+      """
+      generate a sequence of n one_hot encoded characters based on initial 
+      hidden state h0 and input character x0
+      
+      Return: n*k matrix of n generated chars encoded as one-hot vectors
+      """
+      p, a, h = self.evaluate(X)
+      char_seq = np.array([self.select_char(pt, unique_chars) for pt in p])
+      return char_seq
     
     def select_char(self, prob, unique_chars):
       """
