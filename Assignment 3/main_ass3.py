42fc16093550d345a2a26e80d1cff9aac43a86ac
Clara Tump <clara.tump@hotmail.com>
2019-06-24 12:51:14 +0100

    Update main_ass3.py
    More checks made and todo's added

--- Assignment 3/main_ass3.py
+++ Assignment 3/main_ass3.py
@@ -12,7 +12,7 @@ Author: Clara Tump
 # keep track of moving average
 # change evaluate so that in test phase the mu and var are given instead of
 # computed from the batch
-# check the gradients
+# check the gradients <-- correct!
 # 
 """
 
@@ -25,6 +25,15 @@ def main():
   Main method. 
   Load in train, validation and test set.
   Then train the neural network (ann) and test the performance. 
+  
+  Add regularization to forward pass.
+  
+  Checks:
+  - gradient correct with and without batch norm and with and without regularization
+  - test acc results from ass2 cannot be reproduced (0.12 after 32 epochs)
+      Possible reasons:
+           - not all data is used (only data batch 1 and maybe 4 more needed)
+           - forgot regularization in forward pass
   """
   num_classes=10
   np.random.seed(12)
@@ -34,18 +43,18 @@ def main():
   X_train, y_train , Y_train = load_data(batch_file = "data_batch_1", k=num_classes)
   X_test, y_test, Y_test = load_data(batch_file = "test_batch")
   X_val, y_val , Y_val = load_data(batch_file = "data_batch_2")
-  ann1 = ANN(X_train[:3,:5], Y_train[:,:5], batch_norm_flag = True)
-  #ann1 = ANN(X_train, Y_train, batch_norm_flag = True)
+  #ann1 = ANN(X_train[:10,:6], Y_train[:,:6], batch_norm_flag = True)
+  ann1 = ANN(X_train, Y_train, batch_norm_flag = False)
   # uncomment this line to check gradients. remember to init ANN with same no. data points / feats
-  ann1.check_gradients(X_train[:3, :5], Y_train[:, :5], method='centered_diff')
+  #ann1.check_gradients(X_train[:10, :6], Y_train[:, :6], method='centered_diff')
   #ann1.train(X_train[:3,:5], Y_train[:,:5], X_val[:3,:5], Y_val[:,:5], verbosity= True)
-  #ann1.train(X_train, Y_train, X_val, Y_val, verbosity= True)
-  #Y_pred_test, act_h_test, _,_,_,_,_ = ann1.evaluate(X_test)
-  #test_acc = ann1.compute_accuracy(Y_pred_test, Y_test)
-  #print("------------------------------------------------------")
-  #print("               FINAL TEST ACCURACY")
-  #print(test_acc)
-  #print("------------------------------------------------------")
+  ann1.train(X_train, Y_train, X_val, Y_val, verbosity= True)
+  Y_pred_test, act_h_test, _,_,_,_,_ = ann1.evaluate(X_test)
+  test_acc = ann1.compute_accuracy(Y_pred_test, Y_test)
+  print("------------------------------------------------------")
+  print("               FINAL TEST ACCURACY")
+  print(test_acc)
+  print("------------------------------------------------------")
 
 
 def load_data(batch_file = "data/data_batch_1", num=None, feat_num=None, k=10):
